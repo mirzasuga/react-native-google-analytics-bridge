f1f274a7e122003cd1dd3b691f4218abd7fb32d2
Mirza <mirza@Mirzas-MacBook-Pro.local>
2019-06-20 13:28:06 +0700

    initial commit

---
+++ examples/preRN56Example/ios/build/example.build/Debug-iphonesimulator/example.build/Objects-normal/i386/example_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/cbrevik/Dev/IH/react-native-google-analytics-bridge/example/ios/build/example.build/Debug-iphonesimulator/example.build/DerivedSources/example_vers.c
---
+++ examples/preRN56Example/ios/build/example.build/Debug-iphonesimulator/example.build/Objects-normal/i386/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/cbrevik/Dev/IH/react-native-google-analytics-bridge/example/ios/example/main.m \
+  /Users/cbrevik/Dev/IH/react-native-google-analytics-bridge/example/ios/example/AppDelegate.h
